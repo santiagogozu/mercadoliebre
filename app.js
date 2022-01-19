ecac642e4745321c30b260e10cc0388a835e057b
santiagogozu <santiagogo1993@gmail.com>
2022-01-18 19:30:45 -0500

    configuracion para Heroku

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+// app.listen(3000, ()=>{
+//     console.log('Servidor funcionando');
+// });
+
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Servidor funcionando');
 });
 
